09fa777addee92c14fb1c3c19ac0a37cee6e2427
Kensuke Fukami <kensuke8293@gmail.com>
2024-02-29 10:10:32 +0900

    delete

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 class Item(BaseModel):
     name: str
     price: float
@@ -25,12 +27,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-@app.get("/neko/{neko_id}")
-def update_neko(neko_id:int):
-    return neko_id*10
